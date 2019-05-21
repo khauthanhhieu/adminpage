eddb84e88611bdd8954d881f05cbe1e80d59451f
khauthanhhieu <thanhhieu021298@gmail.com>
2019-05-21 14:09:47 +0700

    hiển thị danh sách kh

--- controllers/usersControll.js
+++ controllers/usersControll.js
@@ -1,5 +1,12 @@
 var users = require('express');
 
-exports.loadPage = function(req, res) {
-	res.render('user-acc', {title: 'Express' });
+var conn = require('./connection');
+
+exports.loadPage = function (req, res, next) {
+    var sql = "SELECT * FROM users WHERE isdelete=0";
+    conn.query(sql, function (err, users, fields) {
+        if (err) throw err;
+        res.render('user-acc', { title: 'Express', uList: users });
+        //res.end();
+    });
 }
