ffe813cf47d9656b31d33ff588c254bb4ddc6180
khauthanhhieu <thanhhieu021298@gmail.com>
2019-05-21 20:30:02 +0700

    hiển thị ds đơn hàng

--- controllers/ordersControll.js
+++ controllers/ordersControll.js
@@ -1,5 +1,12 @@
-var orders = require('express');
+var users = require('express');
 
-exports.loadPage = function(req, res) {
-	res.render('orders', {title: 'Express' });
+var conn = require('./connection');
+
+exports.loadPage = function (req, res, next) {
+    var sql = "SELECT `fullname`, `email`, `total`, `date`, `stt` FROM users, orders WHERE users.id = orders.user_id";
+    conn.query(sql, function (err, order, fields) {
+        if (err) throw err;
+        res.render('orders', { title: 'Express', oList: order });
+        //res.end();
+    });
 }
