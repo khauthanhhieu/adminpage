c2016f5734841a8ff193d19eaec80a935eb4bac9
khauthanhhieu <thanhhieu021298@gmail.com>
2019-05-28 12:32:08 +0700

    install passport

--- routes/login.js
+++ routes/login.js
@@ -3,5 +3,6 @@ var router = express.Router();
 
 var login_controller = require('../controllers/loginControll');
 router.get('/', login_controller.loadPage);
+router.post('/', login_controller.check);
 
 module.exports = router;
