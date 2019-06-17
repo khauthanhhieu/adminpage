44fa2984241680a58619d34a5a81d488e7986d70
khauthanhhieu <thanhhieu021298@gmail.com>
2019-06-17 21:57:21 +0700

    quản lý trạng thái đơn hàng

--- controllers/ordersControll.js
+++ controllers/ordersControll.js
@@ -3,14 +3,29 @@ var users = require('express');
 var conn = require('./connection');
 
 exports.loadPage = function (req, res, next) {
-    if(req.user){
-    var sql = `SELECT fullname, email, total, DATE_FORMAT(date, "%d/%m/%Y") as odate, stt FROM users, orders WHERE users.id = orders.user_id`;
-    conn.query(sql, function (err, order, fields) {
-        if (err) throw err;
-        res.render('orders', { user:req.user, oList: order });
-        //res.end();
-    });
-}else{
-    res.redirect('/login');
+    if (req.user) {
+        var sql = `SELECT orders.id as id, fullname, email, total, DATE_FORMAT(date, "%d/%m/%Y") as odate, stt FROM users, orders WHERE users.id = orders.user_id`;
+        conn.query(sql, function (err, order, fields) {
+            if (err) throw err;
+            res.render('orders', { user: req.user, oList: order });
+        });
+    } else {
+        res.redirect('/login');
+    }
 }
+
+exports.confirm = function (req, res, next) {
+    var sql = `UPDATE orders SET stt = 2 WHERE id = ?`;
+    conn.query(sql, req.params.id, function (err, order, fields) {
+        if (err) throw err;
+    });
+    res.redirect('/orders');
+}
+
+exports.confirmDelivery = function (req, res, next) {
+    var sql = `UPDATE orders SET stt = 3 WHERE id = ?`;
+    conn.query(sql, req.params.id, function (err, order, fields) {
+        if (err) throw err;
+    });
+    res.redirect('/orders');
 }
--- routes/orders.js
+++ routes/orders.js
@@ -3,5 +3,7 @@ var router = express.Router();
 
 var orders_controller = require('../controllers/ordersControll');
 router.get('/', orders_controller.loadPage);
+router.get('/confirm/:id', orders_controller.confirm);
+router.get('/confirm-delivery/:id', orders_controller.confirmDelivery);
 
 module.exports = router;
